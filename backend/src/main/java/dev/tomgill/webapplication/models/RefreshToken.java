022f8355be068af93f205190a9b7c4c07640efb2
tomggill <tom.gill25@outlook.com>
2023-10-10 14:36:19 +0100

    Implement refresh token

---
+++ backend/src/main/java/dev/tomgill/webapplication/models/RefreshToken.java
@@ -0,0 +1,33 @@
+package dev.tomgill.webapplication.models;
+
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "refresh-tokens")
+public class RefreshToken {
+  @Id
+  private long id;
+
+  @OneToOne
+  @JoinColumn(name = "user_id", referencedColumnName = "id")
+  private User user;
+
+  @Column(nullable = false, unique = true)
+  private String token;
+
+  @Column(nullable = false)
+  private Instant expiryDate;
+}
